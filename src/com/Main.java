8f9d6b66ac158d172eefde3162a79c13eb871941
wangzheng <718652232@qq.com>
2020-08-22 18:04:13 +0800

    更新快排算法

--- src/com/Main.java
+++ src/com/Main.java
@@ -7,34 +7,4 @@ import java.util.*;
  * @date 2020/8/6 7:12 下午
  */
 public class Main {
-    public static void main(String[] args) {
-        int[] a = {1,0,1,0,3};
-        moveZeroes(a);
-    }
-    public  static void moveZeroes(int[] nums) {
-        int k = -1; // 记录第一个0的位置
-        for (int i =0 ;i < nums.length; i++) {
-            int t = nums[i];
-            if (t == 0) {
-                // 如果前面没有出现过0则记录第一个0的位置为当前位置
-                if (k == -1) {
-                    k = i;
-                }
-                continue;
-            }
-            if (k > -1) {
-                // 前面有0，则将当前位置的值与0进行交换，并将0的位置加1即可
-                swarp(nums, i, k);
-                k++;
-            }
-        }
-        System.out.println(nums);
-    }
-
-    private static void swarp(int[] a, int i, int j) {
-        int t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-    }
-
 }
--- src/com/company/base_algorithm/QuickSort.java
+++ src/com/company/base_algorithm/QuickSort.java
@@ -6,47 +6,66 @@ package com.company.base_algorithm;
  */
 public class QuickSort {
     public static void main(String[] args) {
-
-    }
-    public static int[] QuickSort(int[] array, int start, int end) {
-        if (array.length < 1 || start < 0 || end >= array.length || start > end) return null;
-        int smallIndex = partition(array, start, end);
-        if (smallIndex > start)
-            QuickSort(array, start, smallIndex - 1);
-        if (smallIndex < end)
-            QuickSort(array, smallIndex + 1, end);
-        return array;
+        int[] nums = new int[]{1,5,4,6,3,2};
+        quickSort(nums);
     }
     /**
-     * 快速排序算法——partition
+     * 快速排序
      * @param array
-     * @param start
-     * @param end
-     * @return
      */
-    public static int partition(int[] array, int start, int end) {
-        int pivot = (int) (start + Math.random() * (end - start + 1));
-        int smallIndex = start - 1;
-        swap(array, pivot, end);
-        for (int i = start; i <= end; i++)
-            if (array[i] <= array[end]) {
-                smallIndex++;
-                if (i > smallIndex)
-                    swap(array, i, smallIndex);
+    public static void quickSort(int[] array) {
+        int len;
+        if(array == null
+                || (len = array.length) == 0
+                || len == 1) {
+            return ;
+        }
+        sort(array, 0, len - 1);
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * 快排核心算法，递归实现
+     * @param array
+     * @param left
+     * @param right
+     */
+    public static void sort(int[] array, int left, int right) {
+        if(left > right) {
+            return;
+        }
+        // base中存放基准数
+        int base = array[left];
+        int i = left, j = right;
+        while(i != j) {
+            // 顺序很重要，先从右边开始往左找，直到找到比base值小的数
+            while(array[j] >= base && i < j) {
+                j--;
             }
-        return smallIndex;
-    }
 
-    /**
-     * 交换数组内两个元素
-     * @param array
-     * @param i
-     * @param j
-     */
-    public static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+            // 再从左往右边找，直到找到比base值大的数
+            while(array[i] <= base && i < j) {
+                i++;
+            }
+
+            // 上面的循环结束表示找到了位置或者(i>=j)了，交换两个数在数组中的位置
+            if(i < j) {
+                int tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+
+        // 将基准数放到中间的位置（基准数归位）
+        array[left] = array[i];
+        array[i] = base;
+
+        // 递归，继续向基准的左右两边执行和上面同样的操作
+        // i的索引处为上面已确定好的基准值的位置，无需再处理
+        sort(array, left, i - 1);
+        sort(array, i + 1, right);
     }
 
 }
