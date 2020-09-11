5c5ee2e2c69dbaef8f83298eed966000dfd929a5
wangzheng <718652232@qq.com>
2020-09-11 22:23:17 +0800

    更新快排

--- src/com/company/base_algorithm/QuickSort.java
+++ src/com/company/base_algorithm/QuickSort.java
@@ -4,6 +4,10 @@ package com.company.base_algorithm;
  * @author 王峥
  * @date 2020/7/25 3:13 下午
  */
+/*快速排序（Quicksort）是对冒泡排序的一种改进。 时间复杂度nlogn
+        快速排序由C. A. R. Hoare在1960年提出。它的基本思想是：
+        通过一趟排序将要排序的数据分割成独立的两部分，其中一部分的所有数据都比另外一部分的所有数据都要小，
+        然后再按此方法对这两部分数据分别进行快速排序，整个排序过程可以递归进行，以此达到整个数据变成有序序列。*/
 public class QuickSort {
     public static void main(String[] args) {
         int[] nums = new int[]{1,5,4,6,3,2};
